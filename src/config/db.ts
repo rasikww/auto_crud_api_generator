f33bbb8b557406964a4c67ca0183eec5eacef70f
rasikww <rasikauom@gmail.com>
2024-09-06 20:18:33 +0530

    first commit

---
+++ src/config/db.ts
@@ -0,0 +1,17 @@
+import { Pool } from "pg";
+
+const pool = new Pool({
+    connectionString: process.env.DB_CONNECTION_STRING,
+});
+
+async function queryDB(query: string, params: string[]) {
+    const client = await pool.connect();
+    try {
+        const result = await client.query(query, params);
+        return result;
+    } finally {
+        client.release();
+    }
+}
+
+export default queryDB;
